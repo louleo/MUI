85f960e4d307a401578f71d759f6e4e44d37c8ce
Leo Liu <liul@guild.org.au>
2020-07-23 12:14:54 +1000

    Initialise Fluent-UI project and add colors of exchange and neutral.

---
+++ fluent-ui/src/util/colors.ts
@@ -0,0 +1,32 @@
+const exchange = Object.freeze({
+    shade30:"#004578",
+    shade20:"#005a9e",
+    shade10:"#106ebe",
+    primary:"#0078d4",
+    tint10:"#2b88db",
+    tint20:"#c7e0f4",
+    tint30:"#deecf9",
+    tint40:"#eff6fc"
+});
+
+const neutral = Object.freeze({
+    black:"#000000",
+    gray190:"#201f1e",
+    gray160:"#323130",
+    gray150:"#3b3a39",
+    gray130:"#605e5c",
+    gray120:"#797775",
+    gray90:"#a19f9d",
+    gray60:"#c8c6c4",
+    gray50:"#d2d0ce",
+    gray40:"#e1dfdd",
+    gray30:"#edebe9",
+    gray20:"#f3f2f1",
+    gray10:"#faf9f8",
+    white:"#ffffff"
+});
+
+export default Object.freeze({
+    exchange,
+    neutral
+});
