c6bfbdcf1e5e1fa8dd06bde950d71777e6351832
Leo Liu <liul@guild.org.au>
2020-07-28 16:04:42 +1000

    Update colors.

--- fluent-ui/src/util/colors.ts
+++ fluent-ui/src/util/colors.ts
@@ -9,6 +9,39 @@ const exchange = Object.freeze({
     tint40:"#eff6fc"
 });
 
+const excel  = Object.freeze({
+    shade20:'#004b1c',
+    shade10:'#0e5c2f',
+    primary:'#217346',
+    tint10:'#3f8159',
+    tint20:'#4e9668',
+    tint30:'#6eb38a',
+    tint40:'#9fcdb3',
+    tint50:'#e9f5ee'
+});
+
+const onedrive = Object.freeze({
+    shade30:'#004578',
+    shade20:'#005a9e',
+    shade10:'#106ebe',
+    primary:'#0078d4',
+    tint10:'#2b88d8',
+    tint20:'#c7e0f4',
+    tint30:'#deecf9',
+    tint40:'#eff6fc'
+});
+
+const onenote = Object.freeze({
+    shade20:'#4c0f6c',
+    shade10:'#5c1384',
+    primary:'#7719aa',
+    tint10:'#9953c0',
+    tint20:'#d0afe2',
+    tint30:'#ddc5ec',
+    tint40:'#efdffa',
+    tint50:'#f5edfd'
+});
+
 const neutral = Object.freeze({
     black:"#000000",
     gray190:"#201f1e",
@@ -26,7 +59,57 @@ const neutral = Object.freeze({
     white:"#ffffff"
 });
 
+const powerpoint = Object.freeze({
+    shade20:'#740912',
+    shade10:'#a92b1a',
+    primary:'#b7472a',
+    tint10:'#c75033',
+    tint20:'#e86e58',
+    tint30:'#ed9583',
+    tint40:'#fdc9b5',
+    tint50:'#fcf0ed'
+});
+
+const teams = Object.freeze({
+    shade20:'#33344A',
+    shade10:'#464775',
+    primary:'#6264A7',
+    tint10:'#8B8CC7',
+    tint20:'#BDBDE6',
+    tint30:'#E2E2F6',
+    tint40:'#F4F4FC'
+});
+
+const word = Object.freeze({
+    shade20:'#002050',
+    shade10:'#124078',
+    primary:'#2b579a',
+    tint10:'#3c65a4',
+    tint20:'#4a78b0',
+    tint30:'#7da3c6',
+    tint40:'#a5b9d1',
+    tint50:'#e3ecfa'
+});
+
+const yammer = Object.freeze({
+    shade20:'#135995',
+    shade10:'#1554a7',
+    primary:'#106ebe',
+    tint10:'#0078d4',
+    tint20:'#2488d8',
+    tint30:'#69afe5',
+    tint40:'#83d6f2',
+    tint50:'#cce3f5'
+});
+
 export default Object.freeze({
     exchange,
-    neutral
+    neutral,
+    excel,
+    onedrive,
+    onenote,
+    powerpoint,
+    teams,
+    word,
+    yammer
 });
